0814ca0162f1d7072a692033e64856fd1cc87bf3
Andrew Scholer <ascholer@ascholer.org>
2020-01-16 15:50:24 -0800

    W2 Bitwise bugfix

--- Week02/ARMBitwise/clearing.s
+++ Week02/ARMBitwise/clearing.s
@@ -9,8 +9,8 @@ _start:
 
 LDR   r5, =0x1BADDEED
 
-@Clear last three bits, place result into r6
-BIC   r6, r5, #0b111         @or #0x7
+@Clear bits 25-29
+BIC   r6, r5,  #0x3E000000
 
 @Clear bits 16-31 (first two bytes)
 BIC   r7, r5,  #0xFF000000   @clear first byte
